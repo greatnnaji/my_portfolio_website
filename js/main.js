195a5c6d08bf6cf7453dfc6ac1e216ed129bfa25
great nnaji <nnajiuchegreat2004@gmail.com>
2026-01-25 14:57:23 -0500

    fixed error with failed section navigation at the top to swing

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({scrollTop: 0}, 1500, 'swing');
         return false;
     });
     
@@ -43,7 +43,7 @@
             
             $('html, body').animate({
                 scrollTop: $(this.hash).offset().top - 45
-            }, 1500, 'easeInOutExpo');
+            }, 1500, 'swing');
             
             if ($(this).parents('.navbar-nav').length) {
                 $('.navbar-nav .active').removeClass('active');
